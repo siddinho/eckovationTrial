fc3a88d6eb9c48ed11b7bdd08e29eb4b52550ede
siddinho <sidvohra95@live.com>
2016-06-03 18:10:27 +0530

    Initial commit

---
+++ app/src/main/java/com/wordpress/connectwithsid/eckovationtrial/First_AppUI.java
@@ -0,0 +1,41 @@
+package com.wordpress.connectwithsid.eckovationtrial;
+
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class First_AppUI extends Fragment {
+
+
+    public First_AppUI() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+
+
+
+
+
+
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first__app_ui, container, false);
+    }
+
+
+
+}
